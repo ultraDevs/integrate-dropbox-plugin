52ffa7bdd2c5d10467bec5d00e11323bba363ea0
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-04-08 13:20:43 +0600

    icon change svg

--- assets/admin/index.asset.php
+++ assets/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c69e9ac3c6418d09ee2c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '16c500165ec58a7612bd');
